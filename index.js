8db3fd1420dbba90e2a08e6b554c4594c48db4b5
Justin Ochoa <just.och92@gmail.com>
2020-10-23 22:42:52 -0400

    speech app

---
+++ index.js
@@ -0,0 +1,77 @@
+const msgEl = document.getElementById("msg");
+
+const randomNum = getRandomNum();
+
+console.log("Number:", randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// start game
+recognition.start();
+
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+<div>You said: </div>
+<span class="box">${msg}</span>
+`;
+}
+
+function checkNumber(msg) {
+  const num = +msg;
+
+  // check if it is valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += "<div>That is not a valid number</div>";
+    return;
+  }
+
+  //   check if num is in proper range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    return;
+  }
+
+  //   check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+    <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += `
+      '<div>GO LOWER</div>';
+      `;
+  } else {
+    msgEl.innerHTML += `
+      '<div>GO HIGHER</div>';
+      `;
+  }
+}
+
+//generate random number
+function getRandomNum() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Result
+recognition.addEventListener("result", onSpeak);
+
+// End SR service
+recognition.addEventListener("end", () => recognition.start());
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "play-again") {
+    window.location.reload();
+  }
+});
